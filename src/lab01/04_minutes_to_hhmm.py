0973cd8d1346277a9fb060c294f6c621563528fe
al1 <plesenall@gmail.com>
2025-09-23 11:50:17 +0300

    исправ

--- src/lab01/04_minutes_to_hhmm.py
+++ src/lab01/04_minutes_to_hhmm.py
@@ -1,4 +1,11 @@
 minuts = int(input(''))
 hours = minuts // 60 
 min_ost = minuts % 60 
-print(f'{hours}:{min_ost:02d}') 
+days = 0
+if hours > 24 :
+    days = hours // 24
+    hours = hours % 24
+if days >= 1:
+    print(f'{days} дн. {hours}:{min_ost:02d}') 
+else:
+    print(f'{hours}:{min_ost:02d}') 
