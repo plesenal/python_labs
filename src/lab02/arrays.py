554218f10be0c13458dbbd798a42de84452f8668
al1 <plesenall@gmail.com>
2025-09-23 12:19:04 +0300

    new ex

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -1,4 +1,4 @@
-li =list(input().split())
+
 # for i in range(0,len(li)):
 #     if type(li[i]):
 #         if float(li[i]) == int(li[i]):
@@ -6,6 +6,12 @@ li =list(input().split())
 #         else:
 #             li[i] = float(li[i])
 #print(li)
+
+#li = [3, -1, 5, 5, 0]
+#li = [42] 
+#li = [-5, -2, -9]
+#li = []
+#li = [1.5, 2, 2.0, -3.1]
 def min_max(st):
     if len(st):
         ln = []
@@ -18,6 +24,10 @@ def min_max(st):
     else:
         return print('ValueError')
 #min_max(li)
+#li = [3, 1, 2, 1, 3]
+#li = []
+#li = [-1, -1, 0, 2, 2]
+#li = [1.0, 1, 2.5, 2.5, 0]
 def unique_sorted(st):
     ln = []
     for i in range(0,len(st)):
@@ -25,6 +35,21 @@ def unique_sorted(st):
             ln.append(int(float(st[i])))
         else:
             ln.append(float(st[i]))
-    ln = set(ln).sort()
-    return print(list(ln))
-    
+    ln = list(set(ln))
+    ln = sorted(ln)
+    return print(ln)
+#unique_sorted(li)
+#li = [[1, 2], [3, 4]]
+#li = ([1, 2], (3, 4, 5))
+li = [[1], [], [2, 3]]
+#li = [[1, 2], "ab"]
+def flatten(st):
+    st_itog = []
+    for i  in st:
+        if type(i) != str:
+            for s in i :
+                st_itog.append(s)
+        else:
+            return print('TypeError')
+    return print(st_itog)
+flatten(li)
