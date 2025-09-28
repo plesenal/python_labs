064370ecb8594979354fa595a29b79463df0b678
komputer <pl@shsj.dpd>
2025-09-28 22:51:41 +0300

    new 1 ex

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -1,55 +1,28 @@
 
-# for i in range(0,len(li)):
-#     if type(li[i]):
-#         if float(li[i]) == int(li[i]):
-#             li[i] = int(li[i])
-#         else:
-#             li[i] = float(li[i])
-#print(li)
-
-#li = [3, -1, 5, 5, 0]
-#li = [42] 
-#li = [-5, -2, -9]
-#li = []
-#li = [1.5, 2, 2.0, -3.1]
-def min_max(st):
-    if len(st):
-        ln = []
-        for i in range(0,len(st)):
-            if float(st[i]) == int(float(st[i])):
-                ln.append(int(float(st[i])))
-            else:
-                ln.append(float(st[i]))
-        return print(min(ln),max(ln))
+nums = [[3, -1, 5, 5, 0],[42],[-5, -2, -9],[],[1.5, 2, 2.0, -3.1]]
+def min_max(List):
+    if list:
+        return (min(list),max(list))
     else:
-        return print('ValueError')
-#min_max(li)
-#li = [3, 1, 2, 1, 3]
-#li = []
-#li = [-1, -1, 0, 2, 2]
-#li = [1.0, 1, 2.5, 2.5, 0]
-def unique_sorted(st):
-    ln = []
-    for i in range(0,len(st)):
-        if float(st[i]) == int(float(st[i])):
-            ln.append(int(float(st[i])))
-        else:
-            ln.append(float(st[i]))
-    ln = list(set(ln))
-    ln = sorted(ln)
-    return print(ln)
-#unique_sorted(li)
-#li = [[1, 2], [3, 4]]
-#li = ([1, 2], (3, 4, 5))
-li = [[1], [], [2, 3]]
-#li = [[1, 2], "ab"]
-def flatten(st):
+        return ('ValueError')
+for i in nums:
+    print(f"{i} -> {min_max(i)}")
+
+nums = [[3, 1, 2, 1, 3],[],[-1, -1, 0, 2, 2],[1.0, 1, 2.5, 2.5, 0]]
+def unique_sorted(list):
+    return sorted(list(set(listik)))
+for i in nums:
+    print(f'{i} -> {unique_sorted(i)}')
+
+nums = [[[1, 2], [3, 4]],([1, 2], (3, 4, 5)),[[1], [], [2, 3]],[[1, 2], "ab"]]
+def flatten(list):
     st_itog = []
-    for i  in st:
+    for i  in list:
         if type(i) != str:
             for s in i :
                 st_itog.append(s)
         else:
-            return print('TypeError')
-    return print(st_itog)
-flatten(li)
+            return 'TypeError'
+    return st_itog
+for i in nums:
+     print(f'{i} -> {flatten(i)}')
