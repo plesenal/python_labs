f6781b29f817cc0aa95759ba9a30db6f2cb3492e
komputer <pl@shsj.dpd>
2025-09-29 00:11:53 +0300

    2 ex

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -1,21 +1,21 @@
 
 nums = [[3, -1, 5, 5, 0],[42],[-5, -2, -9],[],[1.5, 2, 2.0, -3.1]]
-def min_max(List):
+def min_max(nums):
     if list:
-        return (min(list),max(list))
+        return (min(nums),max(nums))
     else:
         return ('ValueError')
 for i in nums:
     print(f"{i} -> {min_max(i)}")
 
 nums = [[3, 1, 2, 1, 3],[],[-1, -1, 0, 2, 2],[1.0, 1, 2.5, 2.5, 0]]
-def unique_sorted(list):
-    return sorted(list(set(listik)))
+def unique_sorted(nums):
+    return sorted(list(set(nums)))
 for i in nums:
     print(f'{i} -> {unique_sorted(i)}')
 
 nums = [[[1, 2], [3, 4]],([1, 2], (3, 4, 5)),[[1], [], [2, 3]],[[1, 2], "ab"]]
-def flatten(list):
+def flatten(mat):
     st_itog = []
     for i  in list:
         if type(i) != str:
